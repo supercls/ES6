5ac2cf0a9198338b5e8cab4bef50435804da15b0
supercls <649251829@qq.com>
2017-10-25 16:57:36 +0800

    小例子，import执行run.js

---
+++ 1.js
@@ -0,0 +1,10 @@
+// export default function diag(x){
+// 	return x*3
+// }
+export const sqrt = Math.sqrt;// 使用 export{接口} 导出接口， 大括号中的接口名字为上面定义的变量， import和export是对应的；
+export var s1=function(){
+	return 8
+}
+// export default function shw(x){
+// 	return x*7   如果一个js模块文件就只有一个功能， 那么就可以使用export default导出；
+// }
